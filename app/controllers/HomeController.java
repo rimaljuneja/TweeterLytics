5561cb8e75a15d6d265f86a76d44202e453fac1e
azimsurani <azimsurani786@gmail.com>
2020-10-31 06:10:54 +0530

    Inegratred search call with service method,

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,18 +1,37 @@
 package controllers;
 
+import java.util.concurrent.CompletionStage;
+
+import javax.inject.Inject;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import models.Tweet;
+import models.TweetSearchResult;
+import play.libs.Json;
+import play.libs.concurrent.HttpExecutionContext;
 import play.mvc.*;
-import twitter4j.Query;
-import twitter4j.QueryResult;
-import twitter4j.Status;
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
+import services.TweetService;
+import utils.Util;
+
+import static java.util.concurrent.CompletableFuture.supplyAsync;
+
+import java.util.List;
 
 /**
  * This controller contains an action to handle HTTP requests
  * to the application's home page.
  */
 public class HomeController extends Controller {
+
+	private HttpExecutionContext ec;
+
+	@Inject
+	public HomeController(HttpExecutionContext ec) {
+		this.ec = ec;
+	}
+
+
 
 	/**
 	 * An action that renders an HTML page with a welcome message.
@@ -21,28 +40,27 @@ public class HomeController extends Controller {
 	 * <code>GET</code> request with a path of <code>/</code>.
 	 */
 	public Result index() {
-		
-		Twitter twitter = TwitterFactory.getSingleton();
-		
-		Query query = new Query("Dhoni");
-		
-		QueryResult result;
-		
-		try {
-			
-			result = twitter.search(query);
-			
-			for (Status status : result.getTweets()) {
-				
-				System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
-			
-			}
-		} catch (TwitterException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 
 		return ok(views.html.index.render());
+	}
+	
+	public CompletionStage<Result> getTweetsBySearch(String keyword){
+		
+		return supplyAsync(()->{
+			
+			List<Tweet> tweets = TweetService.searchForKeywordAndGetTweets(keyword);
+			
+			// Need cache implementaion
+			// Need Sentiment analysis
+			
+			TweetSearchResult response = new TweetSearchResult(keyword, tweets, "neutral");
+			
+			JsonNode jsonObjects = Json.toJson(response);
+			
+            return ok(Util.createResponse(jsonObjects, true));
+			
+		},ec.current());
+		
 	}
 
 }
