3482b980f9a2a67d545f387f98ebdc67a711487a
azimsurani <azimsurani786@gmail.com>
2020-11-02 01:49:35 +0530

    Caching strategy changed to synccache to avoid use of get method

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,7 +1,6 @@
 package controllers;
 
 import java.util.concurrent.CompletionStage;
-import java.util.concurrent.ExecutionException;
 
 import javax.inject.Inject;
 
@@ -9,7 +8,8 @@ import com.fasterxml.jackson.databind.JsonNode;
 
 import models.Tweet;
 import models.TweetSearchResult;
-import play.cache.AsyncCacheApi;
+
+import play.cache.SyncCacheApi;
 import play.libs.Json;
 import play.libs.concurrent.HttpExecutionContext;
 import play.mvc.*;
@@ -30,15 +30,17 @@ public class HomeController extends Controller {
 
 	private HttpExecutionContext ec;
 
-	private AsyncCacheApi cache;
+	private List<String> cacheKeys;
 
 	@Inject
-	public HomeController(HttpExecutionContext ec,AsyncCacheApi cache) {
+	SyncCacheApi cache;
+
+	@Inject
+	public HomeController(HttpExecutionContext ec,SyncCacheApi cache) {
 		this.ec = ec;
 		this.cache = cache;	
+		cacheKeys = new ArrayList<>();
 	}
-
-
 
 	/**
 	 * An action that renders an HTML page with a welcome message.
@@ -48,7 +50,14 @@ public class HomeController extends Controller {
 	 */
 	public Result index() {
 		
-		cache.removeAll();
+		if(!cacheKeys.isEmpty()) {
+		
+			//clear the cache and start new session for each refresh
+			cacheKeys.parallelStream().forEach(key -> cache.remove(key));
+		
+			cacheKeys = new ArrayList<>();
+		
+		}
 
 		return ok(views.html.index.render());
 	}
@@ -57,42 +66,34 @@ public class HomeController extends Controller {
 
 		return supplyAsync(()->{
 
-			CompletionStage<Optional<List<Tweet>>> cachedTweets = cache.get(keyword.toLowerCase());
-			
+			Optional<List<Tweet>> cachedTweets = cache.get(keyword.toLowerCase());
+
 			List<Tweet> tweets = new ArrayList<>();
-			
-			TweetSearchResult response = null;
 
-			try {
-				
-				if(cachedTweets.toCompletableFuture().get().isPresent()) {
+			if(cachedTweets.isPresent()) {
 
-					tweets = cachedTweets.toCompletableFuture().get().get();
+				tweets = cachedTweets.get();
 
-				}
-
-				else {
-
-					tweets = TweetService.searchForKeywordAndGetTweets(keyword);
-					
-					cache.set(keyword.toLowerCase(), tweets);
-					
-				}
-				
-				//Sentiment Analysis Code
-				
-				response = new TweetSearchResult(keyword, tweets.subList(0, tweets.size() < 10 ? tweets.size() : 10) , "neutral");
-
-				
-			} catch (InterruptedException|ExecutionException e) {
-				
-				e.printStackTrace();
-				
 			}
 
-			JsonNode jsonObjects = Json.toJson(response);
+			else {
 
-			return ok(Util.createResponse(jsonObjects, true));
+				tweets = TweetService.searchForKeywordAndGetTweets(keyword);
+
+				cache.set(keyword.toLowerCase(), tweets);
+
+				cacheKeys.add(keyword.toLowerCase());
+
+			}
+
+			//Sentiment Analysis Code
+
+			TweetSearchResult response = new TweetSearchResult(keyword, tweets.subList(0, tweets.size() < 10 ? tweets.size() : 10) , "neutral");
+
+			JsonNode jsonObject = Json.toJson(response);
+
+			return ok(Util.createResponse(jsonObject, true));
+
 
 		},ec.current());
 
