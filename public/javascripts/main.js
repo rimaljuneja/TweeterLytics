4b03b2e2ef89c137c8ce372571f6b6ee5342e759
Pavit Srivatsan <pavit.sri@gmail.com>
2020-11-02 17:48:28 +0530

    UI implementation

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,24 +1,56 @@
+let txt ="";
+let finaltext = "";
+let count = 1;
 function search() {
-alert("test");
+var searchkey = document.getElementById("searchTerm").value;
+jQuery.ajax({
+            url: "http://localhost:9000/search/" + searchkey,
+            type: "GET",
+
+            contentType: 'application/json; charset=utf-8',
+            success: function(resultData) {
+            	if(count <= 10) {
+            	count++;
+            	      }
+     			 else {
+      			finaltext = "";
+      			txt = "";
+      			count = 1;
+      			}
+      			debugger;
+                var result = JSON.stringify(resultData);
+				var tweets = resultData.data.tweets;
+				 myObj = JSON.parse(JSON.stringify(tweets));
+      			 txt += "<table border='1'>"
+      			 txt += "<caption>"+ "Search terms:" + "<a id=\"" + searchkey + "\"onclick=\"wordStats(" + searchkey + ")\"" +  ">" + searchkey + "</a>  " +
+      			   resultData.data.sentiment +
+      			  "</caption>"
+ 				 for (x in myObj) {
+				   	 txt += "<tr><td>" 
+				   	 + x + "." + "</td>" +
+				   	  "<td id=\"" + myObj[x].userScreenName + "\"onclick=\"displayUser(" + myObj[x].userScreenName + ")\"" +  ">" + myObj[x].userScreenName + "</td>" +
+				   	  "<td>" + myObj[x].tweetText + "</td></tr>";
+				  }
+     			 txt += "</table>"    
+     			 finaltext = txt + finaltext;
+     			 txt = "";
+			     document.getElementById("demo").innerHTML = finaltext;
+            },
+            error : function(jqXHR, textStatus, errorThrown) {
+            },
+
+            timeout: 120000,
+        })
 }
-$(document).ready(function(){
-	$("#photos").jqGrid({
-		url:'http://jsonplaceholder.typicode.com/posts',
-		datatype: "json",
-		colNames:['ID', 'User ID', 'Title', 'Body'],
-		colModel:[
-			{name:'id',index:'id', width:55},
-			{name:'userId',index:'userId', width:55},
-			{name:'title',index:'title', width:300},	
-			{name:'body',index:'body', width:350, sortable:false}		
-		],
-		rowNum:10,
-		loadonce: true,
-		rowList:[10,20,30],
-		pager: '#pager2',
-		sortname: 'id',
-		viewrecords: true,
-		sortorder: "desc",
-		caption:""
-	});
-});
+
+function displayUser(username) {
+if(username.length >1) {
+alert(username[0].textContent);
+}
+else {
+alert(username.textContent);
+}
+}
+function wordStats(keyterm) {
+	alert(keyterm.textContent);
+}
