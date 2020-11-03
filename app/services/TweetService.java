b47dc45be43a4e2e22c028176a38cc4640a1f207
azimsurani <azimsurani786@gmail.com>
2020-11-04 00:37:22 +0530

    Added function to calculate sentiment analysis

--- app/services/TweetService.java
+++ app/services/TweetService.java
@@ -1,7 +1,10 @@
 package services;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import models.Tweet;
@@ -11,8 +14,16 @@ import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 
+/**
+ * @author everyone
+ *
+ */
 public class TweetService {
 
+	/**
+	 * @param keyword
+	 * @return
+	 */
 	public static List<Tweet> searchForKeywordAndGetTweets(String keyword){
 
 		List<Tweet> searchResults = new ArrayList<>();
@@ -25,7 +36,7 @@ public class TweetService {
 		query.resultType(Query.RECENT);
 
 		//count should be changed to 50 for final demo  (50*5=250)
-		query.setCount(1);
+		query.setCount(50);
 
 		int count = 0;
 
@@ -58,6 +69,51 @@ public class TweetService {
 		while( result!=null && (query = result.nextQuery()) !=null);
 
 		return searchResults;
+	}
+	
+	/**
+	 * It calculates the average sentiment of tweets passed.
+	 * @param tweets "List of Tweets"
+	 * @param wordMap "Word Map of postive and negative words"
+	 * @return String sentiment
+	 * @author Azim Surani
+	 */
+	public static String getSentimentForTweets(List<Tweet> tweets,Map<String,Integer> wordMap) {
+		
+		List<String> listOfTweetSentiments = tweets.parallelStream().map(tweet -> {
+			
+			//replace # by space
+			String tweetText = tweet.getTweetText().replace("#", " ");
+			
+			//seperate all words
+			String[] words = tweetText.trim().split("\\s+");
+			
+			//Rrelative tweet sentiment (E.g 2 positive words and 1 negative word then o/p would be 2-1=1... overall positive )
+			Integer sentiment = Arrays.stream(words).map((String word) -> wordMap.getOrDefault(word.toLowerCase() , 0)).reduce(0,(a,b)-> a+b);
+			
+			if(sentiment>0)
+				return "1";
+			else if(sentiment==0)
+				return "0";
+			else
+				return "-1";
+		}).collect(Collectors.toList());
+		
+		//Count of positive tweets
+		int countOfPositiveTweets = Collections.frequency(listOfTweetSentiments,"1");
+		
+		//Count of negative tweets
+		int countOfNegativeTweets = Collections.frequency(listOfTweetSentiments,"-1");
+		
+		if((float)countOfPositiveTweets/tweets.size()>=0.7F)
+			return "positive"; // Return positive if positive tweets are >= 70%
+		
+		else if((float)countOfNegativeTweets/tweets.size()>=0.7F)
+			return "negative"; // Return negative if positive tweets are >= 70%
+		
+		else
+			return "neutral"; //Else return neutral
+		
 	}
 
 }
